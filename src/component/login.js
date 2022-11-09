8f2c92b4c05b1d98b1cf2e4d1ba684c5cd66eef6
amirulasri <amirulasrix@gmail.com>
2022-11-09 19:19:01 +0800

    First init

---
+++ src/component/login.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+import { auth, database } from "firebase";
+import { Button, Spacer, Text, Modal, Container } from "@nextui-org/react";
+import { User } from "react-iconly";
+import "../style/style.css";
+import logo from "../assets/logokuptm.png";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+      errorMessage: "",
+      modalLoginFailed: false
+    }
+  }
+  loginWithGmail = () => {
+    //e.preventDefault();
+    let provider = new auth.GoogleAuthProvider();
+    auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        let additionalUserInfo = result.additionalUserInfo;
+        let user = {
+          userName: result.additionalUserInfo.profile.given_name,
+          profile_picture: result.user.photoURL,
+          fullName: result.user.displayName,
+          email: result.user.email,
+          uid: result.user.uid
+        }
+        if (additionalUserInfo.isNewUser) {
+          this.addUserList(result);
+          this.props.isLogin(user);
+        } else {
+          let userRef = database().ref().child("usersTable").child(result.user.uid);
+          userRef.once("value", snapshot => {
+            var isAvailable = snapshot.val();
+            if (!isAvailable) {
+              this.addUserList(result);
+            } else {
+              userRef.update({ profile_picture: result.user.photoURL, uid: result.user.uid });
+            }
+          });
+          this.props.isLogin(user);
+        }
+      })
+      .catch(error => {
+        var errorMessage = error.message;
+        this.setState({ error: true, errorMessage, modalLoginFailed: true });
+      });
+  };
+
+  addUserList = result => {
+    database()
+      .ref()
+      .child("usersTable")
+      .child(result.user.uid)
+      .set({
+        userName: result.additionalUserInfo.profile.given_name,
+        profile_picture: result.user.photoURL,
+        fullName: result.user.displayName,
+        email: result.user.email,
+        uid: result.user.uid
+      });
+  };
+  
+  render() {
+    return (
+      <div className="loginform">
+        <img src={logo} alt="Logo KUPTM" />
+        <Text h3>KUPTM Chat</Text>
+        <Button
+          onPress={this.loginWithGmail}
+          icon={<User set="bold" />}
+        >
+          <Spacer y={1} />
+          Login with Google
+        </Button>
+        <Modal
+          closeButton
+          aria-labelledby="modal-title"
+          open={this.state.modalLoginFailed}
+          onClose={() => { this.setState({ modalLoginFailed: false }) }}
+        >
+          <Modal.Header>
+            <Text id="modal-title" size={18}>
+              Sign in failed
+            </Text>
+          </Modal.Header>
+          <Modal.Body>
+            <Text>{this.state.errorMessage}</Text>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button auto flat color="error" onClick={() => { this.setState({ modalLoginFailed: false }) }}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+export default Login;
---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDF6kR_vg-IqseFZnk5FQ13ghc0r6ipNYk",
+    authDomain: "react-chat-app-8a5db.firebaseapp.com",
+    projectId: "react-chat-app-8a5db",
+    storageBucket: "react-chat-app-8a5db.appspot.com",
+    databaseURL: "https://react-chat-app-8a5db-default-rtdb.asia-southeast1.firebasedatabase.app",
+    messagingSenderId: "1010903376020",
+    appId: "1:1010903376020:web:1d746c838a9b20e3d11481",
+    measurementId: "G-DNFLSDPRTS"
+};
+
+export {firebaseConfig};
